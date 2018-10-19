e1b8934dd4d265e3732745c57ac80fc635b95328
VladyslavSH <manujlenkovlad2@gmail.com>
2018-10-19 16:22:56 +0300

    Work is changed

---
+++ Lesson 1/Process_Work/Process_Work/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Process_Work
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            foreach (Process process in Process.GetProcesses())
+            {
+                listView1.Items.Add(($"Name: {process.ProcessName} Id: {process.Id}"));
+            }
+        }
+    }
+}
---
+++ Lesson 2/ThreadHW/ThreadHW/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadHW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                
+                    TimerCallback timerCallback = new TimerCallback(GetProcess);
+                    System.Threading.Timer timer = new System.Threading.Timer(timerCallback, null, 0, 10000);
+
+
+        }
+
+        private void GetProcess(object obj)
+        {
+            
+                listView1.Invoke(new Action(() => {
+                    listView1.Items.Clear();
+                    foreach (var process in Process.GetProcesses())
+                    {
+                        listView1.Items.Add($"--Name: {process.ProcessName} - Id {process.Id} ");
+                        listView1.Items.Add($"Count thread: {process.Threads.Count} - Count handle: {process.HandleCount}");
+                        listView1.Items.Add("");
+                    }
+                }));
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] str;
+            foreach (var item in listView1.Items)
+            {
+                File.WriteAllText("New file.txt", item);
+            }
+        }
+    }
+}
