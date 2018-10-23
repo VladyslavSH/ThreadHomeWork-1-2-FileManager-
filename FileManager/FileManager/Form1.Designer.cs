9f924f41dd275b1e5f67d2ef374693cf8e999a63
VladyslavSH <manujlenkovlad2@gmail.com>
2018-10-23 15:59:23 +0300

    Works changed

---
+++ FileManager/FileManager/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(73, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(601, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "D:\\\\";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path:";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Location = new System.Drawing.Point(702, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Back";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(73, 81);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(704, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(73, 110);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(704, 367);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            this.listView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDown_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(405, 55);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(99, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Find out ditailes";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(73, 110);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(704, 367);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 489);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListView listView2;
+    }
+}
+
---
+++ FileManager/FileManager/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        static string buffer = "", bufferItem = "";
+        FileInfo fileInfo = null;
+        DirectoryInfo directoryInfo = null;
+        public Form1()
+        {
+            InitializeComponent();
+            listView2.Visible = false;
+            ToolStripMenuItem toolStripCopy = new ToolStripMenuItem("Copy");
+            toolStripCopy.Click += ToolStripCopy_Click;
+            ToolStripMenuItem toolStripPaste = new ToolStripMenuItem("Paste");
+            toolStripPaste.Click += ToolStripPaste_Click;
+            contextMenuStrip1.Items.AddRange(new[] { toolStripCopy, toolStripPaste });
+        }
+
+        private void ToolStripPaste_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(Psate));
+            t.Start();
+            
+        }
+
+        private void Psate()
+        {
+            fileInfo = new FileInfo(buffer);
+            if (fileInfo.Exists)
+            {
+
+                //fileInfo.CopyTo(Path.Combine(textBox1.Text,bufferItem), true);
+                try
+                {
+                    byte[] arr;
+                    if (buffer.EndsWith(".iso"))
+                    {
+                        
+                        //File.Copy(buffer, Path.Combine(textBox1.Text, bufferItem));
+                        using (FileStream fstream = new FileStream(buffer, FileMode.Open))
+                        {
+                            arr = new byte[fstream.Length/200];
+                            int interval = 0;
+                            progressBar1.Invoke(new Action(() =>
+                            {
+                                progressBar1.Maximum = Convert.ToInt32(fstream.Length/100);
+                            }));
+
+                            for (int i = 0; i < fstream.Length/100; i++)
+                            {
+                                fstream.Read(arr, interval, 100);
+                                interval += 100;
+                                progressBar1.Invoke(new Action(() =>
+                                {
+                                    progressBar1.Value = i + 1;
+                                }));
+                                //Thread.Sleep(100);
+                            }
+                        }
+                        using (FileStream fstrealWrite = new FileStream(Path.Combine(textBox1.Text, bufferItem), FileMode.OpenOrCreate))
+                        {
+                            fstrealWrite.Write(arr, 0, arr.Length);
+                        }
+
+                    }
+                    else
+                    {
+                        using (FileStream fstream = new FileStream(buffer, FileMode.Open))
+                        {
+                            arr = new byte[fstream.Length];
+                            int interval = 0;
+                            progressBar1.Invoke(new Action(() =>
+                            {
+                                progressBar1.Maximum = Convert.ToInt32(fstream.Length / 100);
+                            }));
+
+                            for (int i = 0; i < fstream.Length / 100; i++)
+                            {
+                                fstream.Read(arr, interval, 100);
+                                interval += 100;
+                                progressBar1.Invoke(new Action(() =>
+                                {
+                                    progressBar1.Value = i + 1;
+                                }));
+                                Thread.Sleep(10);
+                            }
+                        }
+                        using (FileStream fstrealWrite = new FileStream(Path.Combine(textBox1.Text, bufferItem), FileMode.OpenOrCreate))
+                        {
+                            fstrealWrite.Write(arr, 0, arr.Length);
+                        }
+                    }
+                   
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    MessageBox.Show("Copy is successful");
+                    progressBar1.Invoke(new Action(() =>
+                    {
+                        progressBar1.Value = 0;
+                    }));
+                    
+                }
+                OpenDirectory();
+            }
+        }
+
+        private void progress()
+        {
+            progressBar1.Invoke(new Action(() =>
+            {
+                progressBar1.Maximum = Convert.ToInt32(buffer.Length / 2);
+            }));
+            for (int i = 0; i < buffer.Length / 2; i++)
+            {
+
+                progressBar1.Invoke(new Action(() =>
+                {
+                    progressBar1.Value = i + 1;
+                }));
+                Thread.Sleep(1000);
+            }
+        }
+        
+        private void ToolStripCopy_Click(object sender, EventArgs e)
+        {
+            buffer = Path.Combine(textBox1.Text, listView1.SelectedItems[0].Text);
+            bufferItem = listView1.SelectedItems[0].Text;
+
+        }
+
+        private void OpenDirectory()
+        {
+            listView1.Invoke(new Action(()=> {
+            listView1.Items.Clear();
+            }));
+            directoryInfo = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo[] directory = directoryInfo.GetDirectories();
+            FileInfo[] files = directoryInfo.GetFiles();
+            foreach (var item in directory)
+            {
+                listView1.Invoke(new Action(() => {
+                listView1.Items.Add(item.ToString());
+                }));
+            }
+            foreach (var Ifile in files)
+            {
+                listView1.Invoke(new Action(() => {
+                listView1.Items.Add(Ifile.ToString());
+                }));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenDirectory();
+            
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text[textBox1.Text.Length - 1] == '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                    while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                    {
+                        textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                    }
+                }
+                else
+                {
+                    while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                    {
+                        textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                    }
+                }
+                OpenDirectory();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                listView2.Visible = true;
+                listView2.Clear();
+                listView2.View = View.Details;
+                listView2.GridLines = true;
+                listView2.Columns.Add("Name");
+                listView2.Columns.Add("Last Access");
+                listView2.Columns.Add("Creation Date");
+                listView2.Columns.Add("Extension");
+                DirectoryInfo directory = new DirectoryInfo(textBox1.Text);
+                foreach (var dir in directory.GetDirectories())
+                {
+                    ListViewItem listViewItem = new ListViewItem(dir.Name);
+                    listViewItem.SubItems.Add(directoryInfo.LastAccessTime.ToShortTimeString());
+                    listViewItem.SubItems.Add(directoryInfo.CreationTime.ToShortDateString());
+                    listView2.Items.Add(listViewItem);
+                }
+                foreach (var file in directory.GetFiles())
+                {
+
+                    ListViewItem listViewItem = new ListViewItem(file.Name);
+                    listViewItem.SubItems.Add(file.LastAccessTime.ToShortTimeString());
+                    listViewItem.SubItems.Add(file.CreationTime.ToShortTimeString());
+                    listViewItem.SubItems.Add(file.Length.ToString());
+                    listViewItem.SubItems.Add(file.Extension);
+                    listView2.Items.Add(listViewItem);
+                }
+
+            }
+            else listView2.Visible = false;
+        }
+
+        
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (Directory.Exists(Path.Combine(textBox1.Text, listView1.SelectedItems[0].Text)))
+            {
+               
+                textBox1.Text = Path.Combine(textBox1.Text, listView1.SelectedItems[0].Text);
+                listView1.Items.Clear();
+                DirectoryInfo directoryInfo = new DirectoryInfo(textBox1.Text);
+                DirectoryInfo[] directory = directoryInfo.GetDirectories();
+                FileInfo[] files = directoryInfo.GetFiles();
+                foreach (var item in directory)
+                {
+                    listView1.Items.Add(item.ToString());
+                }
+                foreach (var Ifile in files)
+                {
+                    listView1.Items.Add(Ifile.ToString());
+                }
+
+            }
+            else if (File.Exists(Path.Combine(textBox1.Text, listView1.SelectedItems[0].Text)))
+            {
+                Process.Start(Path.Combine(textBox1.Text, listView1.SelectedItems[0].Text));
+            }
+        }
+
+        private void listView1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                listView1.ContextMenuStrip = contextMenuStrip1;
+
+            }
+        }
+        
+    }
+}
